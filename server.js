3f5c9b45ce4047253995aa44eca240d05939578f
billygotclout <williamsoluwademilade@gmail.com>
2024-02-19 09:32:24 +0100

    cors

--- server.js
+++ server.js
@@ -6,8 +6,14 @@ const connectDB = require("./config/dbConnect");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
+const cors = require("cors");
 
 connectDB();
+var corsOptions = {
+  // origin: [CLIENT_URL],
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(errorHandler);
 app.use("/api/auth", require("./routes/user.routes"));
